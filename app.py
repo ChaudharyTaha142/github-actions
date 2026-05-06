edfaae356aab877b9eaa66cf039fbc0bd90ad0e0
Muhammad Taha Azhar <tazhar235@gmail.com>
2026-05-06 08:34:25 +0500

    Fix string formatting and consistency in app.py routes

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return render_template('index.html')
 
-@app.route('/healthy')
+@app.route("/")
+def hello_world():
+    return render_template("index.html")
+
+
+@app.route("/healthy")
 def healthy():
-    return 'server is healthy'
+    return "server is healthy"
